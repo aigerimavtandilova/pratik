521623069174146de5d85586d8a3f775eae09c58
Aigerim <aygerim083@gmail.com>
2022-10-24 23:00:47 +0300

    commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int number=5;
+        System.out.println(number);
+        int number1=4;
+        System.out.println(number1);
+        System.out.println(number+number1);
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
